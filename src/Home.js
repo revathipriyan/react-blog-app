8d726a2a70f1b2477bd69ecb71bca15be12bee1e
revathi <revathipriyan18@gmail.com>
2024-04-09 14:13:34 +0530

    dummy json data added.

--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,7 @@ import { useEffect, useState } from "react";
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs,setBlogs] = useState([
-        {title:'Retrofit uses', body:'lorem psodjkf..',author: 'Revathi P',id :1},
-        {title:'Paper uses', body:'sdsd lorem psodjkf..',author: 'Maya',id :2},
-        {title:'Birthday cards', body:'hello lorem psodjkf..',author: 'Varsha',id :3},
-
-    ]);
+    const [blogs,setBlogs] = useState(null);
 
     const [name, setName] = useState("marios");
 
@@ -17,12 +12,18 @@ const Home = () => {
     }
     
     useEffect(()=> {
-        console.log("use effect ran")
-    },[name]);
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            }).then((data) => {
+                console.log(data);
+                setBlogs(data);
+            });
+    },[]);
 
     return ( 
         <div className="home">
-         <BlogList blogs={blogs} title="All blogs" handleDeleteItem={handleDeleteItem}></BlogList>
+         {blogs && <BlogList blogs={blogs} title="All blogs" handleDeleteItem={handleDeleteItem}></BlogList>}
           <p>{name }</p>
           <button onClick={()=> setName("DUa")} className="chnage-name">CHnage name</button>
         </div>
