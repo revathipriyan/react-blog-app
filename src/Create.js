889bba5158e34908ec4a98a2c3f9468b5ffbc8cc
revathi <revathipriyan1818@gmail.com>
2024-04-12 01:15:15 +0530

    new blog form created

--- src/Create.js
+++ src/Create.js
@@ -1,15 +1,30 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+
     return ( <div className="create">
         <h2>Add a New Blog</h2>
         <form >
             <label >Blog title:</label>
-            <input type="text" required/>
+            <input 
+            type="text" 
+            required 
+            value={title} 
+            onChange={(event) => setTitle(event.target.value)}/>
 
             <label >Blog Body:</label>
-            <textarea required/>
+            <textarea 
+            required
+            value={body}
+            onChange={(e)=>setBody(e.target.value)}/>
 
             <label >Blog author:</label>
-            <select>
+            <select
+            value={author}
+            onChange={(e)=>setAuthor(e.target.value)}>
                 <option value="mario">mario</option>
                 <option value="tina">tina</option>
             </select>
