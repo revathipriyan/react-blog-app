0d6081deb0191566d96745c3f8c830c854ffc8d0
revathi <revathipriyan1818@gmail.com>
2024-04-12 01:39:31 +0530

    not found page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -20,6 +21,9 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
                <BlogDetails></BlogDetails>
+            </Route>
+            <Route path="*">
+              <NotFound></NotFound>
             </Route>
         </Switch>
 
